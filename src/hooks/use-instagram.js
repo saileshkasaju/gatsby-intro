90287387fe8630b4aa0b66a40efd38e8b3824cb1
saileshkasaju <leosailesh@gmail.com>
2019-05-14 01:43:59 +0545

    integrate instagram

---
+++ src/hooks/use-instagram.js
@@ -0,0 +1,29 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const useInstagram = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allInstaNode(limit: 12) {
+        nodes {
+          id
+          caption
+          username
+          image: localFile {
+            sharp: childImageSharp {
+              fluid(maxWidth: 120, maxHeight: 120) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return data.allInstaNode.nodes.map(node => ({
+    ...node,
+    ...node.image.sharp,
+  }));
+};
+
+export default useInstagram;
