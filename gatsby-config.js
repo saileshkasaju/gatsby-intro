624f204bf4a39dc824b323608198cef9f58cdf01
saileshkasaju <leosailesh@gmail.com>
2019-05-14 02:12:23 +0545

    add analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,14 @@ module.exports = {
         username: 'gatsbyjs',
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
