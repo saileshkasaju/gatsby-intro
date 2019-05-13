45d87368456c05554752f76dc9e6d73dede31eb2
saileshkasaju <leosailesh@gmail.com>
2019-05-14 01:02:16 +0545

    blog with image setup done

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,31 @@ module.exports = {
       'A Site we built together doing a full-day Frontend Masters Gatsby workshop',
   },
 
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        defaultLayouts: require.resolve('./src/components/layout.js'),
+        gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'images',
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import usePosts from '../hooks/use-posts';
 import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Minosotta</p>
-    <Link to="/about/">Learn about me &larr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+  return (
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
+  );
+};
